7cd6d5c84465f03a44033641ff5fc62db03a2276
ChaneHaDa <chane.ha.da@gmail.com>
2025-04-17 23:55:29 +0900

    initial commit

---
+++ src/main/java/com/iroff/support_lab_backend/SupportLabBackendApplication.java
@@ -0,0 +1,13 @@
+package com.iroff.support_lab_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportLabBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportLabBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iroff/support_lab_backend/SupportLabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iroff.support_lab_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportLabBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
