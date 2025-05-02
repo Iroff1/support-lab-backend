e97ef4237e08658e78d77d5f57055d36fc441741
성필 <philip000412@gmail.com>
2025-05-02 15:20:29 +0900

    refactor: 패키지명 언더바(_) 제거

--- src/main/java/com/iroff/supportlab/SupportLabBackendApplication.java
+++ src/main/java/com/iroff/supportlab/SupportLabBackendApplication.java
@@ -1,4 +1,4 @@
-package com.iroff.support_lab_backend;
+package com.iroff.supportlab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
