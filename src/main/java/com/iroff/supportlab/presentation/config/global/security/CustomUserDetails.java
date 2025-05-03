54bee580432bab2b0021204ec2c86db7c2ef4a59
ChaneHaDa <chane.ha.da@gmail.com>
2025-05-03 23:33:47 +0900

    feat: UserDetails Service 추가

---
+++ src/main/java/com/iroff/supportlab/presentation/config/global/security/CustomUserDetails.java
@@ -0,0 +1,39 @@
+package com.iroff.supportlab.presentation.config.global.security;
+
+import com.iroff.supportlab.presentation.user.out.persistence.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+class CustomUserDetails implements UserDetails {
+
+    private final UserEntity user;
+
+    public UserEntity getUserEntity() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(() -> "ROLE_" + user.getRole().name());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override public boolean isAccountNonExpired()     { return true; }
+    @Override public boolean isAccountNonLocked()      { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled()               { return true; }
+}
