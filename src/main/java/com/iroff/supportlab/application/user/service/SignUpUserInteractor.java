99ddd22600f4535538168af5bc139e95bfa8191d
성필 <philip000412@gmail.com>
2025-05-05 16:10:04 +0900

    feat: 입력 파라미터 검증을 위한 Validator 생성
    - Validator를 각 UseCase마다 상속받게 하여 검증 로직을 구현하도록 했습니다.

--- src/main/java/com/iroff/supportlab/application/user/service/SignUpUserInteractor.java
+++ src/main/java/com/iroff/supportlab/application/user/service/SignUpUserInteractor.java
@@ -8,8 +8,15 @@ import com.iroff.supportlab.domain.user.port.in.SignUpUserUseCase;
 
 @Service
 public class SignUpUserInteractor implements SignUpUserUseCase {
+
 	@Override
 	public SignUpUserResponse signUp(SignUpUserRequest request) {
 		return null;
+	}
+
+	@Override
+	public boolean validate(SignUpUserRequest request) {
+		// throw new DomainException(UserError.EMAIL_ALREADY_EXISTS); // Use Case 에서는 DomainException을 던짐
+		return true;
 	}
 }
--- src/main/java/com/iroff/supportlab/domain/user/port/in/SignUpUserUseCase.java
+++ src/main/java/com/iroff/supportlab/domain/user/port/in/SignUpUserUseCase.java
@@ -2,7 +2,8 @@ package com.iroff.supportlab.domain.user.port.in;
 
 import com.iroff.supportlab.application.user.dto.SignUpUserRequest;
 import com.iroff.supportlab.application.user.dto.SignUpUserResponse;
+import com.iroff.supportlab.domain.common.port.in.Validator;
 
-public interface SignUpUserUseCase {
+public interface SignUpUserUseCase extends Validator<SignUpUserRequest> {
 	SignUpUserResponse signUp(SignUpUserRequest request);
 }
