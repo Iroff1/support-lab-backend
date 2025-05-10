b24457cf36294e737046098f701cb98c3127f687
ChaneHaDa <chane.ha.da@gmail.com>
2025-05-10 18:35:55 +0900

    feat: 회원가입 request, response 추가

--- src/main/java/com/iroff/supportlab/application/user/dto/SignUpUserRequest.java
+++ src/main/java/com/iroff/supportlab/application/user/dto/SignUpUserRequest.java
@@ -1,4 +1,9 @@
 package com.iroff.supportlab.application.user.dto;
 
-public record SignUpUserRequest() {
+public record SignUpUserRequest(
+	String email,
+	String password,
+	String name,
+	String phone
+) {
 }
--- src/main/java/com/iroff/supportlab/application/user/dto/SignUpUserResponse.java
+++ src/main/java/com/iroff/supportlab/application/user/dto/SignUpUserResponse.java
@@ -1,4 +1,9 @@
 package com.iroff.supportlab.application.user.dto;
 
-public record SignUpUserResponse() {
+public record SignUpUserResponse(
+    Long id,
+    String email,
+    String name,
+    String phone
+) {
 }
