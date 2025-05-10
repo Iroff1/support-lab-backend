8a2b927b92548558427c14d02dd0e1ee14237674
ChaneHaDa <chane.ha.da@gmail.com>
2025-05-10 18:36:17 +0900

    feat: api 엔드 포인트 수정 user -> users

--- src/main/java/com/iroff/supportlab/adapter/user/in/web/UserController.java
+++ src/main/java/com/iroff/supportlab/adapter/user/in/web/UserController.java
@@ -17,7 +17,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
 
 	private final SignUpUserUseCase signUpUserUseCase;
