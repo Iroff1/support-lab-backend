06c3a1fb6f78dc578f176779b2949cf32e986585
ChaneHaDa <chane.ha.da@gmail.com>
2025-05-12 14:07:54 +0900

    feat: email 발신자 추가

--- src/main/java/com/iroff/supportlab/adapter/email/out/persistence/GmailSendEmailAdapter.java
+++ src/main/java/com/iroff/supportlab/adapter/email/out/persistence/GmailSendEmailAdapter.java
@@ -1,23 +1,26 @@
 package com.iroff.supportlab.adapter.email.out.persistence;
 
-import com.iroff.supportlab.domain.email.port.out.SendEmailPort;
-import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
+
+import com.iroff.supportlab.domain.email.port.out.SendEmailPort;
+
+import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class GmailSendEmailAdapter implements SendEmailPort {
 
-    private final JavaMailSender mailSender;
+	private final JavaMailSender mailSender;
 
-    @Override
-    public void sendEmail(String to, String subject, String content) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(content);
-        mailSender.send(message);
-    }
+	@Override
+	public void sendEmail(String to, String subject, String content) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("iroff.lab");
+		message.setTo(to);
+		message.setSubject(subject);
+		message.setText(content);
+		mailSender.send(message);
+	}
 } 
