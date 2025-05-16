3d43291c3c8739f673e57811b02f3efc914a52f3
ChaneHaDa <chane.ha.da@gmail.com>
2025-05-16 15:44:09 +0900

    feat: jwt 관련 실패시 작업 추가

--- src/main/java/com/iroff/supportlab/adapter/config/global/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/iroff/supportlab/adapter/config/global/security/JwtAuthenticationEntryPoint.java
@@ -1,4 +1,33 @@
 package com.iroff.supportlab.adapter.config.global.security;
 
-public class JwtAuthenticationEntryPoint {
+import java.io.IOException;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+	@Override
+	public void commence(
+		HttpServletRequest request,
+		HttpServletResponse response,
+		AuthenticationException authException
+	) throws IOException, ServletException {
+
+		log.warn("Unauthorized error: {}", authException.getMessage());
+
+		response.setContentType("application/json;charset=UTF-8");
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+		response.getWriter().write(
+			"{ \"error\": \"Unauthorized\", \"message\": \"" + authException.getMessage() + "\" }"
+		);
+	}
 }
