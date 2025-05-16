e6826109278b3eda7bd47981520309213cafb8ed
ChaneHaDa <chane.ha.da@gmail.com>
2025-05-16 15:43:12 +0900

    feat: login 엔드포인트 추가

--- src/main/java/com/iroff/supportlab/adapter/auth/in/web/AuthController.java
+++ src/main/java/com/iroff/supportlab/adapter/auth/in/web/AuthController.java
@@ -1,4 +1,26 @@
 package com.iroff.supportlab.adapter.auth.in.web;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.iroff.supportlab.application.auth.dto.LoginRequest;
+import com.iroff.supportlab.application.auth.dto.LoginResponse;
+import com.iroff.supportlab.domain.auth.port.in.AuthUseCase;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
+	private final AuthUseCase authUseCase;
+
+	@PostMapping("/login")
+	public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+		LoginResponse response = authUseCase.login(request);
+		return ResponseEntity.ok(response);
+	}
 }
