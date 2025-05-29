5fe9c6d6566a2b3035464703407dc758ec444079
성필 <philip000412@gmail.com>
2025-05-29 21:39:42 +0900

    fix: 이메일 찾기 시에도 전화번호 인증을 하도록 수정

--- src/main/java/com/iroff/supportlab/application/auth/dto/SendCodeRequest.java
+++ src/main/java/com/iroff/supportlab/application/auth/dto/SendCodeRequest.java
@@ -1,10 +1,15 @@
 package com.iroff.supportlab.application.auth.dto;
 
 import com.iroff.supportlab.adapter.auth.in.web.validation.KoreanPhone;
+import com.iroff.supportlab.domain.auth.model.vo.VerificationType;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record SendCodeRequest(
+	@NotNull(message = "검증 타입은 필수입니다.")
+	VerificationType type,
+
 	@NotBlank(message = "휴대전화 번호는 필수입니다.")
 	@KoreanPhone
 	String phone
