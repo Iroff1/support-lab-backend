f15f8e3713a3221a2eef9b449b66af382ab1ff80
성필 <philip000412@gmail.com>
2025-06-10 21:06:42 +0900

    feat: 프론트 호스트 CORS 설정 추가

--- src/main/java/com/iroff/supportlab/framework/config/web/WebConfig.java
+++ src/main/java/com/iroff/supportlab/framework/config/web/WebConfig.java
@@ -1,15 +1,22 @@
 package com.iroff.supportlab.framework.config.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	@Value("${web.host}")
+	private String host;
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
 			.allowedOrigins("http://localhost:3000")
+			.allowedOrigins("http://localhost:8001")
+			.allowedOrigins(host)
+			.allowedOrigins("")
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 			.allowedHeaders("*")
 			.allowCredentials(true); // 쿠키 필요 시
