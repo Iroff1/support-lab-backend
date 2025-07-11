979ac48cc8150de30e8e2aac22903065ec4fcace
성필 <philip000412@gmail.com>
2025-07-11 21:56:11 +0900

    fix: Redis에 저장할 Key를 더 계층적으로 분리

--- src/main/java/com/iroff/supportlab/adapter/auth/out/persistence/RedisVerificationCodeRepository.java
+++ src/main/java/com/iroff/supportlab/adapter/auth/out/persistence/RedisVerificationCodeRepository.java
@@ -15,37 +15,46 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RedisVerificationCodeRepository implements VerificationCodeRepository {
 
+	private static final String KEY_PREFIX = "verification-code:";
 	private final StringRedisTemplate redis;
+
+	private String generateKey(VerificationType type, String target) {
+		return KEY_PREFIX + type.getValue() + ":" + target;
+	}
+
+	private String generateKey(VerificationType type, String target, Long userId) {
+		return KEY_PREFIX + type.getValue() + ":" + target + ":" + userId;
+	}
 
 	@Override
 	public void save(VerificationType type, String phone, String code, Duration ttl) {
-		redis.opsForValue().set(type.getValue() + phone, code, ttl);
+		redis.opsForValue().set(generateKey(type, phone), code, ttl);
 	}
 
 	@Override
 	public void saveByUserId(VerificationType type, String phone, Long userId, String code, Duration ttl) {
-		redis.opsForValue().set(type.getValue() + phone + "-" + userId, code, ttl);
+		redis.opsForValue().set(generateKey(type, phone, userId), code, ttl);
 	}
 
 	@Override
 	public Optional<String> find(VerificationType type, String phone) {
-		String code = redis.opsForValue().get(type.getValue() + phone);
+		String code = redis.opsForValue().get(generateKey(type, phone));
 		return Optional.ofNullable(code);
 	}
 
 	@Override
 	public Optional<String> findByUser(VerificationType type, String phone, Long userId) {
-		String code = redis.opsForValue().get(type.getValue() + phone + "-" + userId);
+		String code = redis.opsForValue().get(generateKey(type, phone, userId));
 		return Optional.ofNullable(code);
 	}
 
 	@Override
 	public void remove(VerificationType type, String phone) {
-		redis.delete(type.getValue() + phone);
+		redis.delete(generateKey(type, phone));
 	}
 
 	@Override
 	public void removeByUser(VerificationType type, String phone, Long userId) {
-		redis.delete(type.getValue() + phone + "-" + userId);
+		redis.delete(generateKey(type, phone, userId));
 	}
 }
--- src/main/java/com/iroff/supportlab/adapter/auth/out/persistence/RedisVerificationStateRepository.java
+++ src/main/java/com/iroff/supportlab/adapter/auth/out/persistence/RedisVerificationStateRepository.java
@@ -15,37 +15,46 @@ import lombok.RequiredArgsConstructor;
 public class RedisVerificationStateRepository implements VerificationStateRepository {
 
 	private static final String VERIFIED_VALUE = "true";
+	private static final String KEY_PREFIX = "verification-state:";
 	private final StringRedisTemplate redis;
+
+	private String generateKey(VerificationType type, String target) {
+		return KEY_PREFIX + type.getValue() + ":" + target;
+	}
+
+	private String generateKey(VerificationType type, String target, Long userId) {
+		return KEY_PREFIX + type.getValue() + ":" + target + ":" + userId;
+	}
 
 	@Override
 	public void markedVerified(VerificationType type, String phone, Duration ttl) {
-		redis.opsForValue().set(type.getValue() + phone, VERIFIED_VALUE, ttl);
+		redis.opsForValue().set(generateKey(type, phone), VERIFIED_VALUE, ttl);
 	}
 
 	@Override
 	public void markedVerifiedByUser(VerificationType type, String phone, Long userId, Duration ttl) {
-		redis.opsForValue().set(type.getValue() + phone + "-" + userId, VERIFIED_VALUE, ttl);
+		redis.opsForValue().set(generateKey(type, phone, userId), VERIFIED_VALUE, ttl);
 	}
 
 	@Override
 	public boolean isVerified(VerificationType type, String phone) {
-		String isVerified = redis.opsForValue().get(type.getValue() + phone);
-		return isVerified != null && isVerified.equals(VERIFIED_VALUE);
+		String isVerified = redis.opsForValue().get(generateKey(type, phone));
+		return VERIFIED_VALUE.equals(isVerified);
 	}
 
 	@Override
 	public boolean isVerifiedByUser(VerificationType type, String phone, Long userId) {
-		String isVerified = redis.opsForValue().get(type.getValue() + phone + "-" + userId);
-		return isVerified != null && isVerified.equals(VERIFIED_VALUE);
+		String isVerified = redis.opsForValue().get(generateKey(type, phone, userId));
+		return VERIFIED_VALUE.equals(isVerified);
 	}
 
 	@Override
 	public void remove(VerificationType type, String phone) {
-		redis.delete(type.getValue() + phone);
+		redis.delete(generateKey(type, phone));
 	}
 
 	@Override
 	public void removeByUser(VerificationType type, String phone, Long userId) {
-		redis.delete(type.getValue() + phone + "-" + userId);
+		redis.delete(generateKey(type, phone, userId));
 	}
 }
