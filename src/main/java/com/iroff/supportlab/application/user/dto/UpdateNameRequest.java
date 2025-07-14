09948ba81ab2937e13df564c949b3b109d7312c7
성필 <philip000412@gmail.com>
2025-07-14 20:34:39 +0900

    fix: Swagger 표기 오류 수정

--- src/main/java/com/iroff/supportlab/application/user/dto/UpdateNameRequest.java
+++ src/main/java/com/iroff/supportlab/application/user/dto/UpdateNameRequest.java
@@ -3,7 +3,7 @@ package com.iroff.supportlab.application.user.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
-@Schema(name = "사용자 이름 변경 요청 DTO")
+@Schema(description = "사용자 이름 변경 요청 DTO")
 public record UpdateNameRequest(
 	@Schema(description = "새로운 이름")
 	@NotBlank(message = "이름은 필수입니다.")
--- src/main/java/com/iroff/supportlab/application/user/dto/UpdatePhoneNumberRequest.java
+++ src/main/java/com/iroff/supportlab/application/user/dto/UpdatePhoneNumberRequest.java
@@ -4,7 +4,7 @@ import com.iroff.supportlab.adapter.auth.in.web.validation.KoreanPhone;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(name = "회원 전화번호 수정 요청 DTO")
+@Schema(description = "회원 전화번호 수정 요청 DTO")
 public record UpdatePhoneNumberRequest(
 	@Schema(description = "수정할 전화번호", example = "01012345678")
 	@KoreanPhone
